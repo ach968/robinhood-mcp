e4178aca45c4b85743a35137463f1fb8920b0095
Andrew Chen <andrewkkchen@gmail.com>
2026-02-11 14:18:08 -0600

    feat: add watchlists, news, and fundamentals models

--- robin_stocks_mcp/models/__init__.py
+++ robin_stocks_mcp/models/__init__.py
@@ -1,6 +1,9 @@
 from .market import Quote, Candle
 from .options import OptionContract
 from .portfolio import PortfolioSummary, Position
+from .watchlists import Watchlist
+from .news import NewsItem
+from .fundamentals import Fundamentals
 
 __all__ = [
     "Quote",
@@ -8,4 +11,7 @@ __all__ = [
     "OptionContract",
     "PortfolioSummary",
     "Position",
+    "Watchlist",
+    "NewsItem",
+    "Fundamentals",
 ]
